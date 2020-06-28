f6c398c0f8a988d1e9c2a1916c560aa150be798c
JumarySanchez <sanchezjumary@gmail.com>
2020-06-28 19:18:24 -0400

    backwards for loop

--- main.js
+++ main.js
@@ -50,7 +50,10 @@ const students = [
 console.log(callChallengeBanner(1))
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
-
+for(let i=grades.length-1; i>=0; i--){
+  console.log(grades[i])
+}
+// OR console.log(grades.reverse()) works
 
 // Challenge 3
 // Console.log out only the even numbers in the following array.
